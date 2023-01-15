4db5dcac4d7217da26d3d8acc0a741832818c7ff
Nihilistzsche <nihilistzsche@gmail.com>
2023-01-15 10:20:28 -0800

    Create miniature uranium buildings too and just set the next_upgrade to those.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,11 @@
 if mods["mini-machines"] then
-	data.raw["assembling-machine"]["mini-refinery-1"].next_upgrade = nil
-	data.raw["assembling-machine"]["mini-chemplant-1"].next_upgrade = nil
-	data.raw["storage-tank"]["mini-tank-1"].next_upgrade = nil
+	require("__mini-machines__/func")
+	miniaturize("assembling-machine", "uranium-oil-refinery", "mini-uranium-refinery", 1)
+	data.raw["assembling-machine"]["mini-refinery-1"].next_upgrade = "mini-uranium-refinery-1"
+	miniaturize("assembling-machine", "uranium-chemical-plant", "mini-uranium-chemplant", 1)
+	data.raw["assembling-machine"]["mini-chemplant-1"].next_upgrade = "mini-uranium-chemplant-1"
+	miniaturize("storage-tank", "uranium-storage-tank", "mini-uranium-tank", 1)
+	data.raw["storage-tank"]["mini-tank-1"].next_upgrade = "mini-uranium-tank-1"
 	data.raw["mining-drill"]["mini-miner-1"].fast_replaceable_group = "electric-mining-drill"
 end
 if mods["micro-machines"] then
