3b9c4f1bb517ce555d56aa2bfd76ef3614dc43a9
Nihilistzsche <nihilistzsche@gmail.com>
2023-01-24 22:30:34 -0800

    Clean up for release.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,31 +1,63 @@
 if mods["mini-machines"] then
 	require("__mini-machines__/func")
-	miniaturize("assembling-machine", "uranium-oil-refinery", "mini-uranium-refinery", 1)
-	data.raw["assembling-machine"]["mini-refinery-1"].next_upgrade = "mini-uranium-refinery-1"
-	miniaturize("assembling-machine", "uranium-chemical-plant", "mini-uranium-chemplant", 1)
-	data.raw["assembling-machine"]["mini-chemplant-1"].next_upgrade = "mini-uranium-chemplant-1"
-	miniaturize("storage-tank", "uranium-storage-tank", "mini-uranium-tank", 1)
-	data.raw["storage-tank"]["mini-tank-1"].next_upgrade = "mini-uranium-tank-1"
-	data.raw["mining-drill"]["mini-miner-1"].fast_replaceable_group = "electric-mining-drill"
-	for i = 1, 10 do
-		if data.raw["assembling-machine"]["mini-furnace-"..i] then
-			data.raw["assembling-machine"]["mini-furnace-"..i].fast_replaceable_group = "furnace"
+	if mods["leighzeruranimuminnovations"] then
+		miniaturize("assembling-machine", "uranium-oil-refinery", "mini-uranium-refinery", 1)
+		data.raw["assembling-machine"]["mini-refinery-1"].next_upgrade = "mini-uranium-refinery-1"
+		miniaturize("assembling-machine", "uranium-chemical-plant", "mini-uranium-chemplant", 1)
+		data.raw["assembling-machine"]["mini-chemplant-1"].next_upgrade = "mini-uranium-chemplant-1"
+		miniaturize("storage-tank", "uranium-storage-tank", "mini-uranium-tank", 1)
+		data.raw["storage-tank"]["mini-tank-1"].next_upgrade = "mini-uranium-tank-1"
+		data.raw["mining-drill"]["mini-miner-1"].fast_replaceable_group = "electric-mining-drill"
+	end
+
+	local function find_furnace(name)
+		if data.raw.furnace[name] then
+			return data.raw.furnace[name]
+		end
+		if data.raw["assembling-machine"][name] then
+			return data.raw["assembling-machine"][name]
+		end
+		if data.raw[name] then
+			return data.raw[name]
 		end
 	end
-	data.raw["assembling-machine"]["electric-steel-furnace"].next_upgrade = "mini-furnace-1"
-	data.raw["assembling-machine"]["steel-furnace"].next_upgrade = "mini-furnace-1"
-	local btbl = data.raw["assembling-machine"]["steel-furnace"]
-	data.raw["assembling-machine"]["mini-furnace-1"].collision_box = table.deepcopy(btbl.collision_box)
-	data.raw["assembling-machine"]["mini-furnace-1"].selection_box = table.deepcopy(btbl.selection_box)
-	for i = 2, 10 do
-		if data.raw["assembling-machine"]["mini-furnace-"..i] then
-			local tbl = data.raw["assembling-machine"]["mini-furnace-"..i]
-			tbl.collision_box = table.deepcopy(btbl.collision_box)
-			tbl.selection_box = table.deepcopy(btbl.selection_box)
-			tbl.next_upgrade = "mini-furnace-"..(i - 1)
+
+	for i = 1, 10 do
+		local furnace = find_furnace("mini-furnace-" .. i)
+		if furnace then
+			furnace.fast_replaceable_group = "furnace"
 		end
+	end
+	if mods["Electric Furnaces"] then
+		find_furnace("electric-steel-furnace").next_upgrade = "mini-furnace-1"
+	end
+	find_furnace("steel-furnace").next_upgrade = "mini-furnace-1"
+	local btbl = find_furnace("steel-furnace")
+	for i = 1, 10 do
+		local furnace = find_furnace("mini-furnace" .. i)
+		if furnace then
+			furnace.collision_box = table.deepcopy(btbl.collision_box)
+			furnace.selection_box = table.deepcopy(btbl.selection_box)
+			if i < 10 and find_furnace("mini-furnace" .. (i + 1)) then
+				furnace.next_upgrade = "mini-furnace-" .. (i + 1)
+			end
+		end
+	end
+	if data.raw["beacon"]["mini-beacon-2"] then
+		data.raw["beacon"]["mini-beacon-1"].collision_mask =
+			table.deepcopy(data.raw["beacon"]["mini-beacon-2"].collision_mask)
 	end
 end
 if mods["micro-machines"] then
 	data.raw["mining-drill"]["micro-miner-1"].fast_replaceable_group = "electric-mining-drill"
+	if data.raw["mining-drill"]["micro-miner-10"] then
+		data.raw["mining-drill"]["micro-miner-10"].next_upgrade = nil
+	end
+	if data.raw["becon"]["micro-beacon-10"] then
+		data.raw["beacon"]["micro-beacon-10"].next_upgrade = nil
+	end
+	if data.raw["beacon"]["micro-beacon-2"] then
+		data.raw["beacon"]["micro-beacon-1"].collision_mask =
+			table.deepcopy(data.raw["beacon"]["micro-beacon-2"].collision_mask)
+	end
 end
