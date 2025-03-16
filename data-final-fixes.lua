a23ec23ce4f3b9bde461811331dc7717e8f7d49c
Nihilistzsche <nihilistzsche@gmail.com>
2025-03-16 16:37:13 -0700

    Factorio 2.0 update

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,15 +1,4 @@
 if mods["mini-machines"] then
-	require("__mini-machines__/func")
-	if mods["leighzeruranimuminnovations"] then
-		miniaturize("assembling-machine", "uranium-oil-refinery", "mini-uranium-refinery", 1)
-		data.raw["assembling-machine"]["mini-refinery-1"].next_upgrade = "mini-uranium-refinery-1"
-		miniaturize("assembling-machine", "uranium-chemical-plant", "mini-uranium-chemplant", 1)
-		data.raw["assembling-machine"]["mini-chemplant-1"].next_upgrade = "mini-uranium-chemplant-1"
-		miniaturize("storage-tank", "uranium-storage-tank", "mini-uranium-tank", 1)
-		data.raw["storage-tank"]["mini-tank-1"].next_upgrade = "mini-uranium-tank-1"
-		data.raw["mining-drill"]["mini-miner-1"].fast_replaceable_group = "electric-mining-drill"
-	end
-
 	local function find_furnace(name)
 		if data.raw.furnace[name] then
 			return data.raw.furnace[name]
@@ -34,7 +23,7 @@ if mods["mini-machines"] then
 	find_furnace("steel-furnace").next_upgrade = "mini-furnace-1"
 	local btbl = find_furnace("steel-furnace")
 	for i = 1, 10 do
-		local furnace = find_furnace("mini-furnace" .. i)
+		local furnace = find_furnace("mini-furnace-" .. i)
 		if furnace then
 			furnace.collision_box = table.deepcopy(btbl.collision_box)
 			furnace.selection_box = table.deepcopy(btbl.selection_box)
@@ -53,7 +42,7 @@ if mods["micro-machines"] then
 	if data.raw["mining-drill"]["micro-miner-10"] then
 		data.raw["mining-drill"]["micro-miner-10"].next_upgrade = nil
 	end
-	if data.raw["becon"]["micro-beacon-10"] then
+	if data.raw["beacon"]["micro-beacon-10"] then
 		data.raw["beacon"]["micro-beacon-10"].next_upgrade = nil
 	end
 	if data.raw["beacon"]["micro-beacon-2"] then
